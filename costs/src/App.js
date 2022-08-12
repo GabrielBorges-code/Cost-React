595d9e46b8281d8e82404f41795df61074f5534a
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-08-11 23:31:03 -0300

    aula 23 finalizada

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,8 +1,37 @@
+import React from 'react';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import Container from './components/layout/Container';
+import Footer from './components/layout/Footer';
+import NavBar from './components/layout/NavBar';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <p>Cost</p>
-    </div>
+
+    <Router >
+      <NavBar />
+
+      <Container customClass="min_height">
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route path='/projects' element={<Projects/>}></Route>
+          <Route path='/company' element={<Company/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/newproject' element={<NewProject/>}></Route>
+        </Routes>
+      </Container>
+
+      <Footer />
+
+    </Router>
+    
+ 
   );
 }
 
