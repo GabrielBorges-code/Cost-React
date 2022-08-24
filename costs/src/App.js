efe3c87df8a0b6d3b21f023c4a1d860930a780fd
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-08-23 21:55:36 -0300

    aula 31 finalizada

--- costs/src/App.js
+++ costs/src/App.js
@@ -4,6 +4,7 @@ import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Container from './components/layout/Container';
 import Footer from './components/layout/Footer';
@@ -24,6 +25,7 @@ function App() {
           <Route path='/company' element={<Company/>}></Route>
           <Route path='/contact' element={<Contact/>}></Route>
           <Route path='/newproject' element={<NewProject/>}></Route>
+          <Route path='/project/:id' element={<Project/>}></Route>
         </Routes>
       </Container>
 
